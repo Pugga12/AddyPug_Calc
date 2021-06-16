3c0fd94faff465a37f800ce0c64ac7c7f4c2bc78
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-16 16:09:06 -0400

    Update Console Output

--- AddyPug_Calc_Main.cpp
+++ AddyPug_Calc_Main.cpp
@@ -8,7 +8,7 @@ int main()
     double result = 0.0;
     char oper = '+';
 
-    cout << "AddyPug Calc (C++, V1.6)" << endl << endl;
+    cout << "AddyPug Calc (C++, V1.6, Windows 64-bit Edition)" <<endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b, aXb or axb | a/b | x^y"
         << endl;
 
