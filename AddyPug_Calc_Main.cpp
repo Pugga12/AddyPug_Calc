3f2d1e3772efd440cdd77d13bad2f18503ef57f0
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-08-16 11:52:55 -0400

    Target Win32 For Compatibility

--- AddyPug_Calc_Main.cpp
+++ AddyPug_Calc_Main.cpp
@@ -1,3 +1,20 @@
+/*
+*  Copyright © 2021 NotAddyPug
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
 #include <iostream>
 #include "Math.h"
 using namespace std;
@@ -8,18 +25,15 @@ int main()
     double result = 0.0;
     char oper = '+';
 
-    cout << "AddyPug Calc (C++, V1.6, Windows 64-bit Edition)" <<endl;
+    cout << "AddyPug Calc (v1.6.1 32bit)" <<endl;
+    cout << "Copyright 2021 NotAddyPug. Licensed Under Apache 2.0" << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b, aXb or axb | a/b | x^y"
         << endl;
 
     Calculator c;
-    while (true)
-    {
-        cin >> x >> oper >> y;
-        result = c.Calculate(x, oper, y);
-        cout << "Result is: " << result << endl;
-        break;
-    }
+    cin >> x >> oper >> y;
+    result = c.Calculate(x, oper, y);
+    cout << "Result is: " << result << endl;
     return 0;
 }
 
